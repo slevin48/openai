9ff2dd29845fd0aa3bfb186fc6cc48c243e8544b
slevin48 <yann.debray@gadz.org>
2023-03-02 04:36:13 +0100

    ChatGPT like bot

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,20 @@ import streamlit as st
 # Set the API key for the openai package
 openai.api_key = st.secrets['OPEN_AI_KEY']
 
-st.title("🤖 GPT Chat ")
+st.title("🤖 ChatGPT-like bot 🐱 ")
 
 # Create a text input widget in the Streamlit app
-prompt = st.text_input("Enter your message:")
+prompt = st.text_input("Enter your message:", "Hello!" )
 
 # Generate a response from the ChatGPT model
-completion = openai.Completion.create(
+completion = openai.ChatCompletion.create(
     engine="text-davinci-002",
-    prompt=prompt,
-    max_tokens=1024,
-    n=1,
-    stop=None,
-    temperature=0.5,
+    model="gpt-3.5-turbo",
+      messages=[
+          {"role": "user", "content": prompt }
+            ]
 )
 
 # Display the response in the Streamlit app
-st.write("ChatGPT:", completion.choices[0].text)
+st.write("ChatGPT:", completion.choices[0].message)
 
