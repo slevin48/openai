1d6236a92ae65244a71bbbc5a6c41fc77affe1f5
Slevin48 <yann.debray@gadz.org>
2023-03-05 16:53:42 -0500

    Create New Chat

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,25 @@
 import openai
-import json
+import json, os
 import streamlit as st
 
 # Set the API key for the openai package
 openai.api_key = st.secrets['OPEN_AI_KEY']
 
-st.title('🐱 ChatGPT-like bot 🤖')
+# Functions
+def new_chat(n):
+  #  file_path = f'chat/convo{n+1}.json'
+  #  open(file_path, 'w').close()
+   st.session_state.convo = []
+   st.session_state.id += 1
 
-st.sidebar.button('New Chat 🐱')
-st.sidebar.write('💬 convo 1')
-
-# Initialization
-if 'convo' not in st.session_state:
-    st.session_state.convo = []
+def save_chat(n):
+  file_path = f'chat/convo{n}.json'
+  with open(file_path,'w') as f:
+    json.dump(st.session_state.convo, f, indent=4)
 
 def dumb_chat():
-  dummy = [{"role": "user", "content": "Hello!"}, 
-          {"role": "assistant", "content": "\n\nHello! How can I assist you today?"}]
+  with open('fake/dummy1.json') as f:
+    dummy = json.load(f)
   return dummy[1]['content']
 
 def chat(text):
@@ -27,22 +30,54 @@ def chat(text):
   )
   return completion.choices[0].message.content
 
+
+# Initialization
+if 'convo' not in st.session_state:
+    st.session_state.convo = []
+
+n = len(os.listdir('chat'))
+if 'id' not in st.session_state:
+    st.session_state.id = n
+
+id = st.session_state.id
+
+
+st.title('🐱 ChatGPT-like bot 🤖')
+
+if st.sidebar.button('New Chat 🐱'):
+   new_chat(n)
+for file in sorted(os.listdir('chat')):
+  st.sidebar.write('💬',file.replace('.json',''))
+# st.sidebar.write(f'💬 convo{id}')
+# st.sidebar.write(f'{n} conversations')
+
+
 # Create a text input widget in the Streamlit app
-prompt = st.text_input('Enter your message:', 'Hello!' )
+prompt = st.text_input(f'convo{st.session_state.id}')
 
-st.session_state.convo.append({'role': 'user', 'content': prompt })
+if prompt:
+  # Append the text input to the conversation
+  st.session_state.convo.append({'role': 'user', 'content': prompt })
 
-# response = dumb_chat()
-response = chat(st.session_state.convo)
+  # Query the chatbot with the complete conversation
+  # response = dumb_chat()
+  response = chat(st.session_state.convo)
 
-st.session_state.convo.append({'role': 'assistant', 'content': response })
+  # Add response to the conversation
+  st.session_state.convo.append({'role': 'assistant', 'content': response })
+
+  save_chat(id)
 
 # Display the response in the Streamlit app
-st.write('🤖 ChatGPT:', response)
+for line in st.session_state.convo:
+    if line['role'] == 'user':
+      st.write('🐱',line['content'])
+    else:
+        st.write('🤖',line['content'])
 
-if st.checkbox('debug'):
-  st.write(st.session_state.convo)
+# Debug
+# st.write(st.session_state.convo)
 
-# Write the chat log to a json file
-with open('chat/convo1.json','w') as f:
-    json.dump(st.session_state.convo, f, indent=4)
+# # Write the chat log to a json file
+# if st.sidebar.button('Save chat 🐱🤖'):
+#   save_chat(id)
