726bf7f59a6398e033c5ce9e670e1233d1d7f2e6
Slevin48 <yann.debray@gadz.org>
2023-04-14 09:33:35 -0400

    Update QA app with gpt-3.5-turbo

--- QA/qa_app.py
+++ QA/qa_app.py
@@ -1,21 +1,13 @@
 import streamlit as st
-# from langchain.document_loaders import TextLoader
-# from langchain.document_loaders import PyPDFLoader
-# from langchain.chains import RetrievalQA
-# from langchain.indexes import VectorstoreIndexCreator
-# from langchain.text_splitter import CharacterTextSplitter
-# from langchain.embeddings import OpenAIEmbeddings
-# from langchain.vectorstores import Chroma
-# from langchain.chat_models import ChatOpenAI
-from llama_index import download_loader, GPTSimpleVectorIndex
+from llama_index import download_loader, GPTSimpleVectorIndex, LLMPredictor, ServiceContext
+from langchain.chat_models import ChatOpenAI
 import urllib.request
 import urllib.parse
 import os
 
 os.environ['OPENAI_API_KEY'] = st.secrets['OPEN_AI_KEY'] 
-
-# chat = ChatOpenAI(model_name='gpt-3.5-turbo')
-
+llm_predictor = LLMPredictor(llm=ChatOpenAI(model_name="gpt-3.5-turbo"))
+service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
 
 try:
     os.mkdir('book')
@@ -38,33 +30,13 @@ def get_book(url):
     urllib.request.urlretrieve(url, "book/"+pdf)
     return pdf
 
-
-# # load document with LangChain
-# @st.cache
-# def load_doc(pdf):
-#     loader = PyPDFLoader("book/"+pdf)
-#     documents = loader.load()
-#     # split the documents into chunks
-#     text_splitter = CharacterTextSplitter(chunk_size=3000, chunk_overlap=0)
-#     texts = text_splitter.split_documents(documents)
-#     # select which embeddings we want to use
-#     embeddings = OpenAIEmbeddings()
-#     # create the vectorestore to use as the index
-#     db = Chroma.from_documents(texts, embeddings)
-#     # expose this index in a retriever interface
-#     retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":3})
-#     # create a chain to answer questions 
-#     qa = RetrievalQA.from_chain_type(
-#     llm=chat, chain_type="stuff", retriever=retriever, return_source_documents=True)
-#     return qa
-
 # load document with LLaMa Index
 @st.cache
 def load_doc(pdf):
     PDFReader = download_loader("PDFReader")
     loader = PDFReader()
     documents = loader.load_data(pdf)
-    index = GPTSimpleVectorIndex.from_documents(documents)
+    index = GPTSimpleVectorIndex.from_documents(documents,service_context=service_context)
     return index
 
 def load_index(file):
@@ -83,15 +55,18 @@ except Exception as e:
 
 # list = os.listdir('book')
 # l = st.selectbox("Select Book",list)
-# qa = load_doc(pdf)
 
 file = 'index\index.json'
-index = load_index(file)
-# index = load_doc(pdf)
+
+if os.path.exists(file):
+    index = load_index(file)
+else:
+    index = load_doc('book/'+pdf)
+    # save index to file
+    index.save_to_disk('index/index.json')
 
 query = st.text_area('Query',"what is Vendor-Oriented Pricing?")
 if st.button('Answer'):
-    # res = qa({"query": query})
     res = index.query(query)
     st.write(res.response)
     st.write("### Sources:")
