a1f56fcafdac276d70e99ff95e69b334a09a18ca
Slevin48 <yann.debray@gadz.org>
2023-07-15 21:45:48 -0400

    Update stream response to chat model

--- stream_response.py
+++ stream_response.py
@@ -4,26 +4,47 @@ import streamlit as st
 
 st.set_page_config(page_title="Stream",page_icon="🤖")
 
+def chat_stream(text):
+  # Generate a response from the ChatGPT model
+  messages = [{"role": "user", "content": text}]
+  completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages= messages,
+        stream = True
+  )
+  return completion 
+
+# def llm_stream(text):
+#     completion = openai.Completion.create(
+#         model="text-davinci-003",
+#         prompt=text,
+#         max_tokens=120, 
+#         # temperature = 0.5,
+#         stream = True)
+#     return completion
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role":"assistant", "content":"How can I help you?"}]
+
 # Set the API key for the openai package
-openai.api_key = st.secrets['OPEN_AI_KEY']
+openai.api_key = st.secrets["OPEN_AI_KEY"]
 
-st.title('🤖 Stream Response')
+st.title("🤖 Stream Response")
+st.write("What is the meaning of life?")
+prompt = st.chat_input("Ask me anything ...")
 
-user_input = st.text_input("You: ",placeholder = "Ask me anything ...", key="input")
-
-if st.button("Submit", type="primary"):
-    # st.markdown("----")
-    res_box = st.empty()
+if prompt:
+    with st.chat_message("user",avatar="🐱"):
+        st.write(prompt)
     report = []
-    # Looping over the response
-    for resp in openai.Completion.create(model='text-davinci-003',
-                                        prompt=user_input,
-                                        max_tokens=120, 
-                                        # temperature = 0.5,
-                                        stream = True):
-        # join method to concatenate the elements of the list 
-        # into a single string, then strip out any empty strings
-        report.append(resp.choices[0].text)
-        result = "".join(report).strip()
-        result = result.replace("\n", "")        
-        res_box.markdown(f'*{result}*') 
+    with st.chat_message("assistant",avatar="🤖"):
+        res_box = st.empty()
+        # Looping over the response
+        for resp in chat_stream(prompt):
+                if resp.choices[0].finish_reason is None:
+                    # join method to concatenate the elements of the list 
+                    # into a single string, then strip out any empty strings
+                    report.append(resp.choices[0].delta.content)
+                    result = "".join(report).strip()
+                    result = result.replace("\n", "")        
+                    res_box.write(result) 
