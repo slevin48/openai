b090a30d7ac60777ceb0e8b3583e1639118c036d
Slevin48 <yann.debray@gadz.org>
2023-11-11 17:22:24 -0500

    Upgrade Image App to Dall-E 3

--- DallE/image_app.py
+++ DallE/image_app.py
@@ -1,23 +1,33 @@
-import openai
+from openai import OpenAI
 import streamlit as st
 
-# Set the API key for the openai package
-openai.api_key = st.secrets['OPEN_AI_KEY']
+# Set the API key for the openai client
+client = OpenAI(api_key = st.secrets['OPEN_AI_KEY'])
+
+model = ["dall-e-3", "dall-e-2"]
+
+size = {"dall-e-3": ["1024x1024", "1792x1024", "1024x1792"], 
+        "dall-e-2": ["256x256", "512x512", "1024x1024"]}
 
 st.title("🤖 OpenAI Image")
 
+m = st.radio("Select Model", model)
+s = st.radio("Select Size", size[m])
 # Create a text input widget in the Streamlit app
-prompt = st.text_input("Enter your message:",
+prompt = st.text_input(f"Prompt {m}:",
     "a funny corgi")
+    
+if st.button("Generate Image"):
 
-# Generate an image from the DALL-E model
-model = openai.Image.create(
-    model="image-alpha-001",
-    prompt=prompt,
-    n=1,
-    size="512x512",
-    response_format="url"
-)
+    # Generate an image from the DALL-E model
+    image = client.images.generate(
+        model=m,
+        prompt=prompt,
+        n=1,
+        size=s,
+        response_format="url"
+    )
 
-# Display the generated image in the Streamlit app
-st.image(model['data'][0]['url'])
+    # Display the generated image in the Streamlit app
+    st.image(image.data[0].url)
+    # st.write(image.data[0].url)
